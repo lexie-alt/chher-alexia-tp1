a9ce22c486d069d80cc728c488d5d2425b7c9057
lexie-alt <alexia.c6318@gmail.com>
2025-09-05 07:01:43 -0400

    scene personnage

---
+++ godot-patisserie-tp1/scenes/check_button.gd
@@ -0,0 +1,6 @@
+extends CheckButton
+@onready var audio_player = $AudioStreamPlayer
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseButton and event.is_pressed():
+		audio_player .play()
